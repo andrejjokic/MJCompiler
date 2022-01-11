2f2eaaae381c7f20583108e8c2653ea5d96943a8
Andrej Jokic <andrej.jokic@AspireTechnology.com>
2022-01-11 14:57:08 +0100

    Dodato generisanje koda za new naredbe

--- Projekat/workspace/MJCompiler/src/rs/ac/bg/etf/pp1/CodeGenerator.java
+++ Projekat/workspace/MJCompiler/src/rs/ac/bg/etf/pp1/CodeGenerator.java
@@ -2,9 +2,16 @@ package rs.ac.bg.etf.pp1;
 
 import rs.ac.bg.etf.pp1.ast.*;
 import rs.etf.pp1.mj.runtime.Code;
+import rs.etf.pp1.symboltable.Tab;
 import rs.etf.pp1.symboltable.concepts.Obj;
 
 public class CodeGenerator extends VisitorAdaptor {
+	
+	private final static int BYTE_LENGTH = 1;
+	private final static int WORD_LENGTH = 4;
+	
+	private final static int INT_PRINT_WIDTH = 5;
+	private final static int BYTE_PRINT_WIDTH = 1;
 	
 	private int mainPC;
 	
@@ -16,22 +23,38 @@ public class CodeGenerator extends VisitorAdaptor {
 	//  			Helpers
 	//====================================================================================
 	
-	private void generateConst(Const cnst, int val) {
-		Obj con = TabExtended.insert(Obj.Con, "$", cnst.struct);
-		con.setLevel(0);
-		con.setAdr(val);
-		
-		Code.load(con);
-	}
-	
 	//====================================================================================
 	//  			Visit Methods
 	//====================================================================================
 	
+	//--------------CONST---------------------------------------------------------------
+	
+	public void visit(NumConst cnst) {
+		Code.loadConst(cnst.getVal());
+	}
+	
+	public void visit(CharConst cnst) {
+		Code.loadConst((int)cnst.getVal().charValue());
+	}
+	
+	public void visit(BoolConst cnst) {
+		Code.loadConst(cnst.getVal() ? 1 : 0);
+	}
+	
+	//--------------DESIGNATOR-----------------------------------------------------------
+	
+	public void visit(Designator designator) {
+		SyntaxNode parent = designator.getParent();
+		
+		if (parent.getClass() == FactorDesignator.class) {		// Part of expression
+			Code.load(designator.obj);
+		}
+	}
+	
 	//--------------METHOD---------------------------------------------------------------
 	
 	public void visit(MethodIdent methodIdent) {
-		// If it is a main function, save the pc
+		// If it is a main function, save the PC
 		if (methodIdent.obj.getName().equals("main")) {
 			this.mainPC = Code.pc;
 		}
@@ -55,18 +78,17 @@ public class CodeGenerator extends VisitorAdaptor {
 		Code.put(Code.return_);
 	}
 	
-	//--------------CONST---------------------------------------------------------------
+	//--------------STATEMENT-----------------------------------------------------------
 	
-	public void visit(NumConst cnst) {
-		generateConst(cnst, cnst.getVal());
-	}
-	
-	public void visit(CharConst cnst) {
-		generateConst(cnst, (int)cnst.getVal().charValue());
-	}
-	
-	public void visit(BoolConst cnst) {
-		generateConst(cnst, cnst.getVal() ? 1 : 0);
+	public void visit(PrintStmt stmt) {
+		// Generate instruction
+		if (stmt.getPrintPars().struct == Tab.charType) {	// Print char
+			Code.loadConst(BYTE_PRINT_WIDTH);
+			Code.put(Code.bprint);
+		} else {											// Print integer/boolean
+			Code.loadConst(INT_PRINT_WIDTH);
+			Code.put(Code.print);
+		}
 	}
 	
 	//--------------DESIGNATOR STATEMENT------------------------------------------------
@@ -80,4 +102,18 @@ public class CodeGenerator extends VisitorAdaptor {
 	//--------------TERM----------------------------------------------------------------
 	
 	//--------------FACTOR--------------------------------------------------------------
+	
+	public void visit(FactorWithNew factor) {
+		// Generate instruction
+		Code.put(Code.new_);
+		Code.put2(WORD_LENGTH * factor.getType().struct.getNumberOfFields());
+	}
+	
+	public void visit(FactorWithNewArray factor) {
+		int b = factor.getType().struct == TabExtended.charType ? 0 : 1;
+		
+		// Generate instruction
+		Code.put(Code.newarray);
+		Code.put(b);
+	}
 }
